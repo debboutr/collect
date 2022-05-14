1e1b8cf66d4d870bb16d9b3e55b7c505676f85ae
debboutr <debboutr@gmail.com>
2022-05-13 20:48:23 -0700

    blacken app

--- app.py
+++ app.py
@@ -6,23 +6,27 @@ from datetime import datetime as dt
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     HOME = str(Path(os.getcwd()).parent / "data")
     context = dict()
-    if request.method == 'POST':
+    if request.method == "POST":
         date = dt.now().strftime("%m-%d")
         try:
-            wage = request.form['wage']
-            bags = request.form['bags']
-            pw = request.form['pw']
+            wage = request.form["wage"]
+            bags = request.form["bags"]
+            pw = request.form["pw"]
             if pw == "47":
-               
-            # import pdb
-            # pdb.set_trace()
+
+                # import pdb
+                # pdb.set_trace()
                 with sql.connect(f"{HOME}/database.db") as con:
                     cur = con.cursor()
-                    cur.execute("INSERT INTO loops (date, wage,bags) VALUES (?,?,?)", (date,wage,bags))
+                    cur.execute(
+                        "INSERT INTO loops (date, wage,bags) VALUES (?,?,?)",
+                        (date, wage, bags),
+                    )
                     con.commit()
                     context["msg"] = "Record successfully added"
             else:
@@ -41,80 +45,78 @@ def home():
     cursor.execute(find_all_bags)
     tot_bags = cursor.fetchone()[0]
 
-    avg_wage=round((tot_wages/tot_bags), 2)
+    avg_wage = round((tot_wages / tot_bags), 2)
 
     con.row_factory = sql.Row
 
     cur = con.cursor()
     cur.execute("select * from loops")
-    rows = cur.fetchall()[-5:];
-    context.update({
-        "rows":rows,
-        "tot_bags":tot_bags,
-        "tot_wages":f"${tot_wages:,}",
-        "avg_wage":avg_wage,
-        "user": session
-        })
-    # print(context)
-
+    rows = cur.fetchall()[-5:]
+    context.update(
+        {
+            "rows": rows,
+            "tot_bags": tot_bags,
+            "tot_wages": f"${tot_wages:,}",
+            "avg_wage": avg_wage,
+            "user": session,
+        }
+    )
     con.close()
     return render_template("home.html", **context)
 
-@app.route('/months', methods=['GET'])
+
+@app.route("/months", methods=["GET"])
 def months():
     HOME = str(Path(os.getcwd()).parent / "data")
     context = dict()
     con = sql.connect(f"{HOME}/database.db")
     cursor = con.cursor()
 
-
     con.row_factory = sql.Row
-    sum_month = """select 
-                        sum(wage) as wages, 
+    sum_month = """select
+                        sum(wage) as wages,
                         sum(bags) as bags,
                         count(*) as days,
-                        substr(date, 1, 2) as Month 
-                    from 
-                        loops 
-                    group by 
+                        substr(date, 1, 2) as Month
+                    from
+                        loops
+                    group by
                         substr(date, 1, 2)"""
     cur = con.cursor()
     cur.execute(sum_month)
     ans = cur.fetchall()
     months = {
-            "01": "January",
-            "02": "February",
-            "03": "March",
-            "04": "April",
-            "05": "May",
-            "06": "June",
-            "07": "July",
-            "08": "August",
-            "09": "September",
-            "10": "October",
-            "11": "November",
-            "12": "December",
-            }
-    context.update({
-        "rows":ans,
-        "months": months,
-        "user": session
-        })
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December",
+    }
+    context.update({"rows": ans, "months": months, "user": session})
 
     con.close()
     return render_template("months.html", **context)
 
-@app.route('/total', methods=['GET', 'POST'])
+
+@app.route("/total", methods=["GET", "POST"])
 def total():
     HOME = str(Path(os.getcwd()).parent / "data")
     con = sql.connect(f"{HOME}/database.db")
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute("select * from loops")
-    rows = cur.fetchall();
-    context = { "rows": rows }
+    rows = cur.fetchall()
+    context = {"rows": rows}
     con.close()
     return render_template("total.html", **context)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
