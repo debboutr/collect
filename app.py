cd67b8eda6d031b8d8aeff0c9d9ec60da83b9d05
debboutr <debboutr@gmail.com>
2022-07-21 22:31:02 -0700

    update formatting, add delete

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import sqlite3 as sql
 from datetime import datetime as dt
 from pathlib import Path
 
-from flask import Flask, flash, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+
+DATABASE = Path(__file__).parent / "data/database.db"
 
 app = Flask(__name__)
 
@@ -21,7 +23,7 @@ def home():
             pw = request.form["pw"]
             date = date if day == "" else day
             if pw == "47":
-                with sql.connect("data/database.db") as con:
+                with sql.connect(DATABASE) as con:
                     cur = con.cursor()
                     cur.execute(
                         "INSERT INTO loops (date, wage,bags) VALUES (?,?,?)",
@@ -34,21 +36,16 @@ def home():
         except:
             con.rollback()
             context["msg"] = "error in insert operation"
-    con = sql.connect("data/database.db")
+    con = sql.connect(DATABASE)
     cursor = con.cursor()
-
     find_wages = "select sum(wage), sum(bags) from loops"
     cursor.execute(find_wages)
     tot_wages, tot_bags = cursor.fetchone()
-
     find_work_days = "select count(*) from loops where bags > 0"
     cursor.execute(find_work_days)
     work_days = cursor.fetchone()[0]
-
     avg_wage = round(tot_wages / tot_bags, 2)
-
     con.row_factory = sql.Row
-
     cur = con.cursor()
     cur.execute("select * from loops")
     rows = cur.fetchall()[-5:]
@@ -56,7 +53,7 @@ def home():
         {
             "rows": rows,
             "tot_bags": tot_bags,
-            "tot_wages": f"${tot_wages:,}",
+            "tot_wages": tot_wages,
             "avg_wage": avg_wage,
             "user": session,
             "total_days": total_days,
@@ -69,19 +66,15 @@ def home():
 
 @app.route("/edit/<num>", methods=["GET", "POST"])
 def edit(num):
-
     context = dict()
-
     if request.method == "POST":
-        # date = dt.now().strftime("%Y-%m-%d")
         try:
             day = request.form["day"]
             wage = request.form["wage"]
             bags = request.form["bags"]
             pw = request.form["pw"]
-            print("day: ", day)
             if pw == "47":
-                with sql.connect("data/database.db") as con:
+                with sql.connect(DATABASE) as con:
                     cur = con.cursor()
                     cur.execute(
                         f"UPDATE loops SET date='{day}', wage={wage}, bags={bags} WHERE id={num}",
@@ -92,13 +85,12 @@ def edit(num):
                 context["msg"] = "PW incorrect"
         except:
             con.rollback()
-            context["msg"] = "error in insert operation"
-    con = sql.connect("data/database.db")
+            context["msg"] = "error in update operation"
+    con = sql.connect(DATABASE)
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute(f"select * from loops where id={num}")
     row = cur.fetchone()
-
     context.update(
         {
             "row": row,
@@ -109,12 +101,26 @@ def edit(num):
     return render_template("edit.html", **context)
 
 
+@app.route("/delete/<num>", methods=["POST"])
+def delete(num):
+    if request.method == "POST":
+        try:
+            with sql.connect(DATABASE) as con:
+                cur = con.cursor()
+                cur.execute(
+                    f"DELETE from loops WHERE id={num}",
+                )
+                con.commit()
+        except:
+            con.rollback()
+    return redirect(url_for("total"))
+
+
 @app.route("/months", methods=["GET"])
 def months():
     context = dict()
-    con = sql.connect("data/database.db")
+    con = sql.connect(DATABASE)
     cursor = con.cursor()
-
     con.row_factory = sql.Row
     sum_month = """select
                         sum(wage) as wages,
@@ -143,17 +149,16 @@ def months():
         "12": "December",
     }
     context.update({"rows": ans, "months": months, "user": session})
-
     con.close()
     return render_template("months.html", **context)
 
 
 @app.route("/total", methods=["GET", "POST"])
 def total():
-    con = sql.connect("data/database.db")
+    con = sql.connect(DATABASE)
     con.row_factory = sql.Row
     cur = con.cursor()
-    cur.execute("select * from loops")
+    cur.execute("select * from loops order by date desc")
     rows = cur.fetchall()
     context = {"rows": rows}
     con.close()
